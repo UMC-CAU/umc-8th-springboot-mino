4142c27babbb7fc2681f9ff713cab70eee613d48
Miging <dnfldpden32@gmail.com>
2025-05-15 18:34:01 +0900

    feat: 쿼리 dsl 적용

---
+++ src/main/java/umc/spring/repository/restaurant/RestaurantRepositoryCustom.java
@@ -0,0 +1,15 @@
+package umc.spring.repository.restaurant;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import umc.spring.domain.Member;
+import umc.spring.domain.enums.MemberStatus;
+import umc.spring.domain.mapping.Restaurant;
+
+
+public interface RestaurantRepositoryCustom  {
+	List<Restaurant>  dynamicQueryWithBooleanBuilder(String name, Float score);
+}
